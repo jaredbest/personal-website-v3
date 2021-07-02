30b6648ac283e38453da3b4bfe3f740db164d8f8
jaredbest <me@jaredbest.com>
2021-07-02 09:09:57 +0200

    Updated blog details.

--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -1,8 +1,7 @@
 import { Helmet } from "react-helmet";
-import axios from "axios";
 import Disqus from "disqus-react";
 import React, { useEffect, useState } from "react";
-import ReactMarkdown from "react-markdown/with-html";
+import MarkDown from "markdown-to-jsx";
 import Layout from "../components/Layout";
 
 function BlogDetails(props) {
@@ -11,14 +10,18 @@ function BlogDetails(props) {
   const blogFile = props.match.params.title;
 
   useEffect(() => {
-    axios.get(require(`../blog/${blogFile}.md`)).then((result) => {
-      setContent(result.data);
-    });
-  }, [content, blogFile]);
+    import(`../blog/${blogFile}.md`)
+      .then((res) => {
+        fetch(res.default)
+          .then((res) => res.text())
+          .then((res) => setContent(res));
+      })
+      .catch((err) => console.log(err));
+  }, [blogFile]);
 
-  const disqusShortname = "jared_best"; // Found in your Disqus.com dashboard.
+  const disqusShortname = "jared_best"; //found in your Disqus.com dashboard
   const disqusConfig = {
-    url: "https://www.jaredbest.com/", // Homepage link of this site.
+    url: "https://www.jaredbest.com/", //Homepage link of this site.
     identifier: blogId,
     title: blogFile,
   };
@@ -26,15 +29,15 @@ function BlogDetails(props) {
   return (
     <Layout>
       <Helmet>
-        <title>Blog of Jared Best | Freelance Creative Technologist</title>
+        <title>Blog Details - Chester React Personal Portfolio Template</title>
         <meta
           name="description"
-          content="Blog of Jared Best | Freelance Creative Technologist"
+          content="Chester React Personal Portfolio Template Blog Details Page"
         />
       </Helmet>
       <div className="mi-blog-details mi-section mi-padding-top mi-padding-bottom">
         <div className="container">
-          <ReactMarkdown source={content} escapeHtml={false}></ReactMarkdown>
+          <MarkDown>{content}</MarkDown>
           <div className="mi-blog-details-comments mt-30">
             <Disqus.DiscussionEmbed
               shortname={disqusShortname}
